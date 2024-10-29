3225c938fdb78021af88b8baed09f61555ece25e
Bolt4Action <109246542+Bolt4Action@users.noreply.github.com>
2024-10-29 14:22:03 +0300

    added animated footer for BoltFolio

--- projects/route/Boltfolio/js/main.js
+++ projects/route/Boltfolio/js/main.js
@@ -2,6 +2,8 @@ let navBar = document.querySelector(".navbar");
 let navBarBrand = document.querySelector(".navbar-brand");
 let dancingText = document.querySelector(".dancing-text");
 let isSwitched = false;
+let navLinks = Array.from(document.querySelectorAll(".nav-link"));
+navLinks.pop();
 
 navBarBrand.addEventListener("mouseover", () => {
     if (!isSwitched) {
@@ -25,9 +27,44 @@ navBarBrand.addEventListener("mouseover", () => {
         }, 5000)
     }
 })
-// dancingText.addEventListener("mouseout", () => {
-//     navBar.classList.remove("navbar-scrolled");
-// })
+
+
+//Animate footer text
+let dancingFooter = document.querySelector(".dancing-footer");
+
+function dancingFooterDisplayBootstrap() {
+    dancingFooter.classList.add("dancing-footer-disabled");
+    setTimeout(() => {
+        dancingFooter.innerHTML = "uilt with Bootstrap"
+        dancingFooter.classList.remove("dancing-footer-disabled");
+    }, 1100)
+}
+
+function dancingFooterDisplayBoltfolio() {
+    dancingFooter.classList.add("dancing-footer-disabled");
+    setTimeout(() => {
+        dancingFooter.innerHTML = "oltFolio"
+        dancingFooter.classList.remove("dancing-footer-disabled");
+    }, 1100)
+}
+
+
+function animatedFooter() {
+    setTimeout(() => {
+        dancingFooterDisplayBootstrap();
+    }, 2000);
+    
+    setTimeout(() => {
+        dancingFooterDisplayBoltfolio();
+    }, 8000);
+}
+
+
+animatedFooter();
+setInterval(() => {
+    animatedFooter();
+}, 10000)
+
 
 let prevScrollpos = window.scrollY;
 
@@ -46,11 +83,28 @@ window.onscroll = function () {
     prevScrollpos = currentScrollPos;
 }
 
-// setInterval(() => {
-//     if (window.scrollY === 0) {
-//         navBar.classList.remove("navbar-scrolled");
-        
-//     }
-//     else {
-//     }
-// }, 1000)
+
+// Change active navbar link on scroll
+
+// console.log(sectionTops);
+
+
+let sections = document.querySelectorAll("section");
+window.addEventListener("scroll", () => {
+    let activeSection;
+    let sectionTops = [];
+    for (let i = 0; i < sections.length; i++) {
+        sectionTops.push(sections[i].getBoundingClientRect().top)
+    }
+    for (let i = 0; i < sectionTops.length; i++) {
+
+        if (sectionTops[i] <= 250) {
+            activeSection = i;
+        }
+    }
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].classList.remove("active");
+    }
+    navLinks[activeSection].classList.add("active");
+})
+
